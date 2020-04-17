09f70323806ba15ea834fd29bb5906e23733762e
UmerSaleem <mr.umersaleem@gmail.com>
2020-04-18 05:22:34 +0800

    Initial Commit

---
+++ src/com/gittest1/Main.java
@@ -0,0 +1,10 @@
+package com.gittest1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("This is GIT Test Project");
+        System.out.println("Things Changed");
+    }
+}
